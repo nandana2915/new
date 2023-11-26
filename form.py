b3d39898354024217d48e22286039b957fe77211
nandana2915 <130975174+nandana2915@users.noreply.github.com>
2023-11-26 20:23:05 +0530

    Add files via upload

---
+++ form.py
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<HEAD>
+<title>
+      WELCOME TO MIT WPU  !!
+</title>
+ <link rel="icon" type="image/x-icon" href="favicon.ico">
+ <style>
+ h1{
+ font-size:40px;
+ text-align: center;
+ }
+ 
+ h2{
+ font-size:20px;
+ text-align: center;
+ }
+ 
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+</style>
+</head>
+	  
+<body bgcolor="white" background="C:\Users\MTECH\Desktop\ADMISSION FORM-20230810T054030Z-001\ADMISSION FORM\abc.jpg" >
+          <img src="C:\Users\MTECH\Desktop\ADMISSION FORM-20230810T054030Z-001\ADMISSION FORM\MIT World Peace University.jpg" width="1200" height="333">
+		  <p style="color:red;font-size:50px;text-align: center;font-family:impact;"> MIT WPU ME AAPKA SWAGAT HAI !! </p>
+		  <p style="color:blue;font-size:20px;text-align: center;font-family:verdana;">TAKE ADMISSION AT YOUR OWN RISK !! </p>
+          <div style="margin: auto;width: 50%;">
+          <form>
+             <h1> STDENT ADMISSION FORM </h1>
+             <h2>FILL BELOW FORM TO TAKE ADMISSION</h2>
+             <br>
+             
+             <label><b>First Name</b></label> 
+             <input type="text" placeholder="Enter your first name" name="first_name" required>
+             <br>
+             
+             <br>
+             <label><b>Last Name</b></label>
+             <input type="text" placeholder="Enter your last name" name="last_name" required>
+             <br>
+             <br>
+             <label><b>E-mail</b></label>
+             <input type="email" placeholder="Enter your e-mail" name="email" required>
+			              <p>for ex. abc@gmail.com</p>
+             <br>
+             <br>
+             <label><b>Date of Birth</b></label>
+             <input type="date" name="dob" required>
+             <br>
+             <br>
+             <label><b>Set Username</b></label>
+             <input type="text" placeholder="Set Username" name="username" required>
+             <br>
+             <br>
+             <label><b>Set Password</b></label>
+             <input type="password" placeholder="Set password" name="password" required>
+             <br>
+             
+             <br>
+             <label><b>Gender</b></label><br>
+             <input type="radio" name="gender" value="Male">
+             <label for="Male">Male</label><br>
+             <input type="radio" name="gender" value="Female">
+             <label for="Female">Female</label><br>
+             <input type="radio" name="gender" value="Others">
+             <label for="Others">Others</label>
+             <br>
+              
+            <br>
+            <label><b>Course :</b></label>   
+                 <select>
+                       <option value="Course">Course</option>    
+                       <option value="CSF">CSE (CYBER SECUIRITY AND FORENSICS)</option>  
+                       <option value="CIVIL">CIVIL ENGINEERING</option>  
+                       <option value="MECH">MECHANICAL ENGINEERING</option>    
+                 </select>
+           <br>
+           <br>
+           <input type="button" value="Register"/> 
+          </form>
+		  
+		  <p><a href="https://www.mitwpu.edu.in/">VISIT OUR OFFICIAL WEBSITE !!</a></p>
+		  
+     </div>
+</body>
+</html>
